2e4d9cf57348572875cdd8fe908697d16a13f09d
Artur Sapek <artur.sapek@gmail.com>
2012-06-06 12:58:00 -0400

    new tests, idk what else to test anymore.

--- testing/tests.js
+++ testing/tests.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
     appid: '84e5c4a381e7424b8df62e055f0b69db',
     apikey: '84c8c3f1223b4710b180d181cd6fb1df'
   });
+  window.cm_bad_apikey = new cloudmine.WebService({
+    appid: '84e5c4a381e7424b8df62e055f0b69db',
+    apikey: 'marc sucks lol'
+  });
+  window.cm_bad_appid = new cloudmine.WebService({
+    appid: 'philly cheese steak',
+    apikey: '84c8c3f1223b4710b180d181cd6fb1df'
+  });
 
   var rL = function(){ // Random letter
     return 'abcdefghijklmnopqrstuvwxyz'.split('')[parseInt(Math.random() * 26)];
@@ -16,9 +24,7 @@ $(document).ready(function(){
 
 
   // Test 1: Register a user, then log them
-/* LEAVE COMMENTED OUT FOR NOW TO NOT SPAM THE DB'S: IT WORKS
-  test("Create user, log in as them", function(){
-    expect(1);
+  test('Create user, log in as them', function(){
     stop();
 
     var user = {
@@ -28,19 +34,17 @@ $(document).ready(function(){
     setTimeout(function(){
       cm.createUser(user.email, user.password).on('success', function(){
         cm.login({username: user.email, password: user.password}).on('success', function(data){
-          ok(data.hasOwnProperty('session_token'), "Has session token");
+          ok(data.hasOwnProperty('session_token'), 'Has session token');
           start();
         });
       });
     }, 13);
   });
-*/
 
 
   // Test 2: Create an object with set(), get that key, see if they match
 
-  test("Create object with set, get it, see if equal", function(){
-    expect(1);
+  test('Create object with set, get it, see if equal', function(){
     stop();
 
 
@@ -54,7 +58,7 @@ $(document).ready(function(){
         };
       cm.set(key, value).on('success', function(){
         cm.get(key).on('success', function(data){
-          deepEqual(value, data[key], "Set then get a value. deepEqual?");
+          deepEqual(value, data[key], 'Set then get a value. deepEqual?');
           start();
         });
       });
@@ -63,8 +67,7 @@ $(document).ready(function(){
 
   // Test 3: Create an object with update(), get that key, see if they match
 
-  test("Create object with update, get it, see if equal", function(){
-    expect(1);
+  test('Create object with update, get it, see if equal', function(){
     stop();
 
     setTimeout(function(){
@@ -77,7 +80,7 @@ $(document).ready(function(){
         };
       cm.update(key, value).on('success', function(){
         cm.get(key).on('success', function(data){
-          deepEqual(value, data[key], "Update then get a value. deepEqual?");
+          deepEqual(value, data[key], 'Update then get a value. deepEqual?');
           start();
         });
       });
@@ -86,8 +89,7 @@ $(document).ready(function(){
 
   // Test 4: Set an object, then update its attributes one at a time to see if they be changin
 
-  test("Set object and test to see if update works on it", function(){
-    expect(4);
+  test('Set object and test to see if update works on it', function(){
     stop();
 
 
@@ -99,38 +101,103 @@ $(document).ready(function(){
           string: '321',
           array: [3, '2', 1],
           object: { '3': 2, '1': 'a' }
-        };
+        },
+        calls_done = 0,
+        register_call = function(){
+          calls_done ++;
+          if (calls_done == 4){
+            setTimeout(function(){ start(); }, 100);
+          }
+        }
 
       cm.set(key, value).on('success', function(){
 
         cm.update(key, {string: '123'}).on('success', function(){
           cm.get(key).on('success', function(data1){
-            equal('123', data1[key].string, "Updated string successfully.");
+            equal('123', data1[key].string, 'Updated string successfully.');
+            register_call();
           });
         });
         cm.update(key, {object: {'3': 1, 'x': '1'}}).on('success', function(){
           cm.get(key).on('success', function(data2){
-            deepEqual({'1': 'a', '3': 1, 'x': '1'}, data2[key].object, "Updated object successfully.");
+            deepEqual({'1': 'a', '3': 1, 'x': '1'}, data2[key].object, 'Updated object successfully.');
+            register_call();
           });
         });
         cm.update(key, {array: [1, '2', 3]}).on('success', function(){
           cm.get(key).on('success', function(data3){
-            deepEqual([1, '2', 3], data3[key].array, "Updated array successfully.");
+            deepEqual([1, '2', 3], data3[key].array, 'Updated array successfully.');
+            register_call();
           });
         });
         cm.update(key, {integer: 123}).on('success', function(){
           cm.get(key).on('success', function(data4){
-            equal(123, data4[key].integer, "Updated integer successfully.");
-            setTimeout(function(){
-              start();
-            }, 000);
+            equal(123, data4[key].integer, 'Updated integer successfully.');
+            register_call();
           });
         });
       });
     }, 13);
   });
 
-   
+
+  test('Set object, delete it, try to get it again.', function(){
+    stop();
+
+    var key = 'test_object4',
+        value = {
+          'A': 'B',
+          'C': 'D'
+        };
+
+    cm.set(key, value).on('success', function(){
+      cm.destroy(key).on('success', function(){
+        cm.get(key).on(404, function(){
+          ok(true, 'Error upon trying to get deleted object');
+          start();
+        });
+      });
+    });
+  });
+
+
+  // Test 6: Let's fuck some shit up
+
+  test('401, 404 errors for object queries with bad API key and App IDs', function(){
+    stop();
+      
+    var key = 'test_object5',
+        error_404 = false,
+        error_401 = false,
+        calls_done = 0,
+        register_call = function(){
+          calls_done ++;
+          if (calls_done == 2){
+            setTimeout(function(){ start(); }, 13);
+          }
+        }
+
+    cm.set(key, {'Britney': 'Spears'});
+
+    cm_bad_apikey.get(key).on(401, function(){
+      error_401 = true;
+      ok(error_401, '401 error fired correctly for apikey "marc sucks lol"');
+      register_call();
+    });
+
+    cm_bad_appid.get(key).on(404, function(){
+      error_404 = true;
+      ok(error_404, '404 error fired correctly for appid "philly cheese steak"');
+      register_call();
+    });
+  });
+    
+  // Note: I can't test the 400 error code for invalid JSON because 
+  //   A) Steven parses it out before I can call it
+  //   or
+  //   B) Javascript bitches about it before I can call it
+
+  
 
 });
 
