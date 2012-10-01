c025507ae5304542acec06a847516ecc0969af71
Steven Berlan <steven@cloudmine.me>
2012-10-01 10:21:23 -0400

    Missed version number bump

--- js/cloudmine.js
+++ js/cloudmine.js
@@ -1,4 +1,4 @@
-/* CloudMine JavaScript Library v0.9.3 cloudmine.me | cloudmine.me/license */ 
+/* CloudMine JavaScript Library v0.9.4 cloudmine.me | cloudmine.me/license */ 
 (function() {
   /**
    * Construct a new WebService instance
