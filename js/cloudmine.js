2e68ee2153352a9019bf7bb7562ff1bb61eac57c
Ethan Mick <ethan@cloudmine.me>
2013-01-29 16:25:22 -0500

    Fixed bug in social login with storing session token and UserID

--- js/cloudmine.js
+++ js/cloudmine.js
@@ -791,15 +791,18 @@
         processResponse: function(data) {
           if (data.finished) {
             self.options.session_token = data.session_token;
+	    self.options.userid = data.profile.__id__;
             data = { success: data }
           }
           else {
             self.options.session_token = null;
+	    self.options.userid = null;
             data = { errors: [ "Login unsuccessful." ] }
           }
 
           if (options.savelogin) {
-            store('session_token', self.options.session_token, self.options.appid);
+            store('session_token', data.success.session_token, self.options.appid);
+	    store('userid', data.success.profile.__id__, self.options.appid);
           }
 
           return data;
