23ac6eb363fe7a933ab0b0f872b6257f8ab12267
Michael Caputo <therubikscubekid@gmail.com>
2015-03-04 11:17:24 -0500

    Updated version numbers

--- js/cloudmine.js
+++ js/cloudmine.js
@@ -1,6 +1,6 @@
 /* CloudMine JavaScript Library v0.9.x cloudmine.me | cloudmine.me/license */
 (function() {
-  var version = '0.9.13-git';
+  var version = '0.9.13';
 
   /**
    * Construct a new WebService instance
